0401add1ecbaa42a6ed3f7b9c7287007e97b1a8c
Rahul <romeydeviller@gmail.com>
2025-09-15 22:19:20 +0530

    for domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/portfolio/', // Uncomment and update this line when deploying to GitHub Pages
+  base: './', // ✅ Important for GitHub Pages - ensures relative paths work correctly
   server: {
     port: 3000,
     open: true,
